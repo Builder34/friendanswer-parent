232b0d03a53d6353bbc8d3abd9a666787b42f838
Builder34 <Builder34@lcbiao34@gmail.com>
2017-02-06 10:56:55 +0800

    web管理后台

---
+++ friendanswer-admin/src/main/java/com/qing/utils/constants/RESTfulUrlConstants.java
@@ -0,0 +1,12 @@
+package com.qing.utils.constants;
+
+/**
+ * Created by Builder34 on 2017/1/11.
+ */
+public class RESTfulUrlConstants {
+    /**
+     * HomeController下的url
+     * */
+    public final static String HOME = "/home" ;
+    public final static String HOME_MAIN = "/main" ;
+}
